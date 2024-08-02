8823054e3312f1231bf95c314467b5894aa2ac06
DCMATE <theoelite31@gmail.com>
2024-08-02 15:17:27 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
+import { defineCustomElements } from '@techfever/astral-ui/loader';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
   title = 'astral-ui-test';
+
+  constructor() {
+    defineCustomElements(window)
+  }
 }
