ecdea817612f3f3762324d9840188ab7da265f23
Théophane VRAUX <theoelite31@gmail.com>
2024-08-02 14:20:31 +0000

    Patched app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
-
+import { CUSTOM_ELEMENTS_SCHEMA, Component, ElementRef, ViewChildren } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { defineCustomElements } from '@techfever/astral-ui/loader';
 
@@ -12,9 +11,61 @@ import { defineCustomElements } from '@techfever/astral-ui/loader';
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
-  title = 'astral-ui-test';
-
   constructor() {
-    defineCustomElements(window)
+    // Define custom elements from the Astral UI library
+    defineCustomElements(window);
+  }
+
+  ngAfterViewInit() {
+    // Find the toggle theme button and its children
+    const toggleThemeButton = document.body.querySelector('#toggle-theme-button') as HTMLElement;
+    const icon = toggleThemeButton?.querySelector('i');
+    const buttonText = toggleThemeButton?.querySelector('#toggle-theme-button-text') as HTMLElement;
+
+    // Function to check if the current theme is dark
+    const isDarkTheme = () => document.documentElement.getAttribute('data-theme') === 'dark';
+
+    // If all elements are present, add event listeners and update the UI
+    if (toggleThemeButton && icon && buttonText) {
+      // Update the button icon and text based on the current theme
+      const updateThemeUI = () => {
+        if (isDarkTheme()) {
+          icon.innerText = 'light_mode';
+          buttonText.innerText = 'Toggle light mode';
+        } else {
+          icon.innerText = 'dark_mode';
+          buttonText.innerText = 'Toggle dark mode';
+        }
+      };
+
+      // Initial UI update based on the current theme
+      updateThemeUI();
+
+      // Add event listener to toggle the theme on button click
+      toggleThemeButton.addEventListener('click', () => {
+        if (isDarkTheme()) {
+          document.documentElement.setAttribute('data-theme', 'light');
+        } else {
+          document.documentElement.setAttribute('data-theme', 'dark');
+        }
+        updateThemeUI();
+      });
+    }
+
+    // Set the initial theme based on the user's system preference
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+    }
+
+    // Listen for changes in the user's system theme preference
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+      if (event.matches) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+      } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+      }
+    });
   }
 }
